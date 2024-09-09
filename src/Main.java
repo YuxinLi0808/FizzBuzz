eac801af6f6d959624f787a06f9a264c64a47d0b
YuxinLi0808 <157935471+YuxinLi0808@users.noreply.github.com>
2024-09-09 10:30:32 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzbuzz(5);
         FizzBuzz.main("fizz".split(""));
+        System.out.println("hello java!");
     }
 }
